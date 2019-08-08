cfad8b1acc0555ddb8f55694be96c8d0b24977df
root <root@localhost.localdomain>
2019-08-08 09:08:24 +0100

    commit

--- app/Sms.php
+++ app/Sms.php
@@ -49,22 +49,25 @@ class Sms extends Model
     }
     public function message($data, $message)
     {
-        $username = 'sandbox'; // use 'sandbox' for development in the test environment
         // $username = 'willow'; // use 'sandbox' for development in the test environment
         $apiKey   = '2fd972d459061d98697de4170eef1bb5137d8f7a6d1c7e3c6a2a924cecacf203'; // use your sandbox app API key for development in the test environment
         // $apiKey   = 'ef0c4a1d3d2b347d2c7d173fb61b3841735bf523e1bbeda938508d782cf20153'; // Live
+
+
+        $username = 'sandbox'; // use 'sandbox' for development in the test environment
         $AT       = new AfricasTalking($username, $apiKey);
         // Get one of the services
         $sms      = $AT->sms();
         // Use the service
         $result   = $sms->send([
-            'from'      => 'willow',
+            'from'    => 'sender',
             'to'      => $data['phone'],
-            'message' => "Dear " . $data['name'] . '. ' . $message,
+            'message' => "Dear " . $data['name'] . ' . ' . $message,
             // 'message' => "Dear " . $data['name'] . ' <br /> ' . $message,
             // 'message' => 'Hello',
         ]);
         print_r($result);
     }
 }
+
 
